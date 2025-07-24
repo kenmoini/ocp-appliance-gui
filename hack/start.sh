af05bb54566b48be18ddb5562366ae948a76b956
Ken Moini <ken@kenmoini.com>
2025-07-23 22:22:48 -0400

    sync

---
+++ hack/start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check for podman and ensure it's available
+if ! command -v podman &> /dev/null; then
+    echo "Podman could not be found. Please install Podman to use this script."
+    exit 1
+fi
+
+# Check for the data path
+if [ ! -d "./gui-data" ]; then
+    echo "Creating gui-data directory..."
+    mkdir -p ./gui-data/{data,graph}
+    chmod 777 ./gui-data
+fi
+
+export GUI_MODE=${1:-"appliance"} # appliance, agent
+export IMAGE_NAME=${IMAGE_NAME:-"ocp-appliance-gui"}
+
+podman run --name ocp-gui \
+  --rm -it \
+  --privileged \
+  -p 8501:8501 \
+  -v ./gui-data/data:/data:Z \
+  -v ./gui-data/graph:/var/lib/containers \
+  -v /run/podman/podman.sock:/run/podman/podman.sock \
+  ${IMAGE_NAME}
