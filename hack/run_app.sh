bfe7e49c900c01fb1f808f701b1f2b3281b43d71
Ken Moini <ken@kenmoini.com>
2025-07-23 23:53:55 -0400

    sync

--- hack/run_app.sh
+++ hack/run_app.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 PROJECT_ROOT=$(realpath "$SCRIPT_DIR/..")
+cd "$PROJECT_ROOT" || exit 1
 
 # Check for Python3 and ensure it's available
 if ! command -v python3 &> /dev/null; then
@@ -9,20 +10,21 @@ if ! command -v python3 &> /dev/null; then
     exit 1
 fi
 
-
 # Check for the data path
-if [ ! -d "${PROJECT_ROOT}/gui-data" ]; then
+if [ ! -d "./gui-data" ]; then
     echo "Creating gui-data directory..."
-    mkdir -p ${PROJECT_ROOT}/gui-data/{data,graph}
-    chmod 777 -R ${PROJECT_ROOT}/gui-data
+    mkdir -p ./gui-data/{data,graph}
+    chmod 777 -R ./gui-data
 fi
 
-python3 -m venv ${PROJECT_ROOT}/gui-data/venv
-source ${PROJECT_ROOT}/gui-data/venv/bin/activate
-python3 -m pip install -r ${PROJECT_ROOT}/setup/requirements.txt
+python3 -m venv ./venv
+source ${PROJECT_ROOT}/venv/bin/activate
+python3 -m pip install -r ./setup/requirements.txt
 
 export GUI_MODE=${1:-"appliance"} # appliance, agent
-export BUILD_BASE_PATH=${BUILD_BASE_PATH:-"./gui-data/data"}
+export BUILD_BASE_PATH=${BUILD_BASE_PATH:-"${PROJECT_ROOT}/gui-data/data"}
+
+cd ${PROJECT_ROOT}/src || exit 1
 
 # Switch between appliance and agent mode
 if [ "$GUI_MODE" == "agent" ]; then
