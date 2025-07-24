2e5ce5d0f14793ea8a1eca55525257d5a304e3cf
Ken Moini <ken@kenmoini.com>
2025-07-23 20:42:48 -0400

    sync

--- src/ui.py
+++ src/ui.py
@@ -191,7 +191,7 @@ if generateISO_button:
 
     # Pull the appliance image
     pull_output = st.empty()
-    pull_response = []
+    pull_response = [""]
 
     appliancePull_cmd = subprocess.Popen(["podman", "pull", os.environ.get('APPLIANCE_IMAGE')], env=process_env, stdout=subprocess.PIPE)
     while appliancePull_cmd.poll() is None:
@@ -222,7 +222,7 @@ if generateISO_button:
     ]
 
     build_output = st.empty()
-    build_response = []
+    build_response = [""]
 
     applianceBuild_cmd = subprocess.Popen(podmanApplianceImageBuild_cmd, env=process_env, stdout=subprocess.PIPE)
     while applianceBuild_cmd.poll() is None:
