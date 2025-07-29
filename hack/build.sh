3df9ebd69167bc54a12a315e59a62e647c84ce58
Ken Moini <ken@kenmoini.com>
2025-07-28 21:59:56 -0400

    sync

--- hack/build.sh
+++ hack/build.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+PROJECT_ROOT=$(realpath "$SCRIPT_DIR/..")
+cd "$PROJECT_ROOT" || exit 1
 
 if ! command -v podman &> /dev/null; then
     echo "Podman could not be found. Please install Podman to use this script."
     exit 1
 fi
 
-podman build -t ocp-appliance-gui .
+if [ ! -z "$1" ]; then
+    export CFILE="$1"
+else
+    export CFILE="default"
+fi
+
+if [ "$CFILE" == "default" ]; then
+    echo "Using default Containerfile file: $CFILE"
+    podman build -t ocp-appliance-gui .
+else
+    echo "Using custom configuration file: $CFILE"
+    podman build -t ocp-appliance-gui -f "$CFILE" .
+fi
